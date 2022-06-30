dd4c746a38fd09d0476842e54c37e9ffcad0d3b4
Emma Chan <echan@credil.org>
2022-06-30 09:59:38 -0400

    created sign in button
    takes you to a page with the credentials of the user

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,21 @@
 class RegistrationsController < ApplicationController
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        render plain: "Thanks!"
+  def create
+    @user = User.new sign_up_path
+    if @user.save
+      redirect_to root_path, notice: "Successfully created account"
+    else
+      render :new
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+    params[:user]
+  end
 end
