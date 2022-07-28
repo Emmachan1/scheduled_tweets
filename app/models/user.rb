61f4c00066af3dab808afd114417270364aecc21
Emma Chan <echan@credil.org>
2022-07-28 15:24:38 -0400

    Twitter Account Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@
 # password:string virtual
 # passwrod_confirmation:string virtual
 class User < ApplicationRecord
-
+    has_many :twitter_accounts
+    
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
