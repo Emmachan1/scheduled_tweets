dfd44077eab99fb71f3efe3fcc44ac51e7b93c56
Emma Chan <echan@credil.org>
2022-08-09 16:23:58 -0400

    Tweet Validations Pt 1 Working on fixing "user does not exist" that pops up when it should not

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,22 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  def create 
+    @tweet = Tweet.create(tweet_params)
+    
+    if @tweet.valid?
+      @tweet.save
+      session[:tweet_id] = @tweet.id
+      retired_to tweets_path, notice: "Tweet was scheduled successfully"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :twitter_account, :body, :publish_at)
+  end
 end
